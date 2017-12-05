7348f285cbc1c9a6c1850b0f1b945fae37fa841a
Jan Koehnlein <jan.koehnlein@typefox.io>
2017-12-05 14:58:05 +0100

    cleaned up context menu

--- go-extension/src/browser/go-menu-contribution.ts
+++ go-extension/src/browser/go-menu-contribution.ts
@@ -5,6 +5,37 @@ import { injectable } from "inversify";
 @injectable()
 export class GoMenuContribution implements MenuContribution {
     registerMenus(menus: MenuModelRegistry): void {
+        // go.toggle.test.file: unmapped
+        menus.registerMenuAction(EDITOR_CONTEXT_MENU.concat("2_go"), {
+            commandId : "go.add.tags",
+            label :"Go: Add Tags To Struct Fields"
+        })
+        menus.registerMenuAction(EDITOR_CONTEXT_MENU.concat("2_go"), {
+            commandId : "go.remove.tags",
+            label : "Go: Remove Tags From Struct Fields"
+        })
+        // go.test.cursor: unmapped
+        // go.benchmark.cursor: unmapped
+        // go.test.file: unmapped
+        // go.test.package: unmapped
+        menus.registerMenuAction(EDITOR_CONTEXT_MENU.concat("2_go"), {
+            commandId : "go.test.generate.function",
+            label : "Go: Generate Unit Tests For Function"
+        })
+        menus.registerMenuAction(EDITOR_CONTEXT_MENU.concat("2_go"), {
+            commandId : "go.test.generate.file",
+            label : "Go: Generate Unit Tests For File"
+        })
+        menus.registerMenuAction(EDITOR_CONTEXT_MENU.concat("2_go"), {
+            commandId : "go.test.generate.package",
+            label : "Go: Generate Unit Tests For Package"
+        })
+        // go.import.add: unmapped as we cannot prompt for a name
+        // go.test.coverage: unmapped
+        // go.show.commands: unmapped TODO
+
+        // additional context menu actions. 
+        // TODO: remove when go.show.commands is active
         menus.registerMenuAction(EDITOR_CONTEXT_MENU.concat("2_go"), {
             commandId: "go.install.package",
             label: "Go: Install Current Package"
@@ -26,28 +57,8 @@ export class GoMenuContribution implements MenuContribution {
             label : "Go: Vet Workspace"
         })
         menus.registerMenuAction(EDITOR_CONTEXT_MENU.concat("2_go"), {
-            commandId : "go.test.generate.package",
-            label : "Go: Generate Unit Tests For Package"
-        })
-        menus.registerMenuAction(EDITOR_CONTEXT_MENU.concat("2_go"), {
-            commandId : "go.test.generate.file",
-            label : "Go: Generate Unit Tests For File"
-        })
-        menus.registerMenuAction(EDITOR_CONTEXT_MENU.concat("2_go"), {
-            commandId : "go.test.generate.function",
-            label : "Go: Generate Unit Tests For Function"
-        })
-        menus.registerMenuAction(EDITOR_CONTEXT_MENU.concat("2_go"), {
             commandId : "go.get.package",
             label : "Go: Get Package"
-        })
-        menus.registerMenuAction(EDITOR_CONTEXT_MENU.concat("2_go"), {
-            commandId : "go.add.tags",
-            label :"Go: Add Tags To Struct Fields"
-        })
-        menus.registerMenuAction(EDITOR_CONTEXT_MENU.concat("2_go"), {
-            commandId : "go.remove.tags",
-            label : "Go: Remove Tags From Struct Fields"
         })
         menus.registerMenuAction(EDITOR_CONTEXT_MENU.concat("2_go"), {
             commandId: "go.playground",
